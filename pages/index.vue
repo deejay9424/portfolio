189a53a89b998f552871a2beebc04c5d5d36c64e
deejay9424 <dhruv.voith@gmail.com>
2019-10-12 22:00:40 +0530

    Firebase hosting Done

--- pages/index.vue
+++ pages/index.vue
@@ -81,12 +81,10 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    Logo
   }
 }
 </script>
